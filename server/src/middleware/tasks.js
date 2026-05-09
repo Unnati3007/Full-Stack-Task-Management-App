9d9be28f736f03b8975a2d00a549ccccc95a25b3
Unnati3007 <168943898+Unnati3007@users.noreply.github.com>
2026-05-09 20:30:40 +0530

    Delete server/src/middleware/tasks.js

--- server/src/middleware/tasks.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { authenticate } = require("../middleware/auth");
-const c = require("../controllers/taskController");
-router.use(authenticate);
-router.get("/",     c.getTasks);
-router.post("/",    c.createTask);
-router.put("/:id",  c.updateTask);
-router.delete("/:id", c.deleteTask);
-module.exports = router;
